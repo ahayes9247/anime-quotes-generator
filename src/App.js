a40ddf290b6bb23ce2aa6ef24460727cd29e3d2d
ahayes9247 <hayes.amanda@outlook.com>
2022-04-09 10:35:18 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect  } from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const url = 'https://animechan.vercel.app/api/random';
 
-export default App;
+export default function App() {
+
+  function refreshPage() {
+    window.location.reload(false);
+
+  };
+
+  const [animeQuotes, setQuotes] = useState([]);
+
+  const getQuotes = async() =>{
+    const response = await fetch(url);
+    const animeQuotes = await response.json();
+
+    setQuotes(animeQuotes);
+  }
+
+    useEffect(() => {
+      getQuotes();
+    }, []);
+
+    return (
+      <div id="quotes__container" class="quotes__class">
+        <div class="anime__quote">
+          <p class="quote">{animeQuotes["quote"]}</p>
+          <p class="quote__information">— <strong>{animeQuotes["character"]}</strong>, <em>{animeQuotes["anime"]}</em></p>
+        </div>
+
+        <div>
+          <p>
+            <button type="button" class="btn btn-primary" onClick={refreshPage}>Get New Quote</button>
+          </p>
+        </div>
+      </div>
+    )
+
+}
